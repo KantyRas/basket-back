f9b3b6c9eb568e91398d7de2a8b2e71f735c2b3d
KantyRas <rasolofomananakanty@gmail.com>
2023-11-21 16:35:47 +0300

    dd

--- base.sql
+++ base.sql
@@ -1,38 +1,172 @@
--- gestion de flotte de vehicule --
+-- classement , joueur , equipe , match equipe , match joue , point par match , rebond par match , minute par match , efficacite , FG , 3 points , lance front
 
--- Création de la table des véhicules
-CREATE TABLE vehicule (
-    idvehicule SERIAL PRIMARY KEY,
+-- Joueur :
+--     id
+--     nom
+--     age
+
+CREATE TABLE joueur(
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
-    immatriculation VARCHAR(20) UNIQUE NOT NULL,
-    annee_fabrication INT,
-    modele VARCHAR(50),
-    couleur VARCHAR(30)
+    age INT NOT NULL
 );
 
--- Création de la table des kilomètres parcourus
-CREATE TABLE kilometres (
-    idkilometrage SERIAL PRIMARY KEY,
-    idvehicule INT REFERENCES vehicule (idvehicule),
-    date DATE NOT NULL,
-    debut_km INT NOT NULL,
-    fin_km INT NOT NULL,
-    CONSTRAINT check_km_range CHECK (debut_km <= fin_km)
+
+
+
+-- Equipe :
+--     id
+--     nom
+
+CREATE TABLE equipe(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL
 );
 
--- Création de la table des assurances
-CREATE TABLE assurances (
-    idassurance SERIAL PRIMARY KEY,
-    idvehicule INT REFERENCES vehicule (idvehicule),
-    date_assurance DATE NOT NULL,
-    montant double precision NOT NULL
+
+-- match :
+--     id
+--     date_heure_debut
+--     date_heure_fin
+
+CREATE TABLE match(
+    id SERIAL PRIMARY KEY,
+    date_heure_debut TIMESTAMP NOT NULL,
+    date_heure_fin TIMESTAMP NOT NULL
 );
 
--- Création de la table des entretiens
-CREATE TABLE entretiens (
-    identretien SERIAL PRIMARY KEY,
-    idvehicule INT REFERENCES vehicule (idvehicule),
-    date_entretien DATE NOT NULL,
-    type_entretien VARCHAR(50),
-    montant double precision NOT NULL
+
+
+-- composition_equipe 
+--     id_match
+--     id_equipe
+--     id_joueur
+
+CREATE TABLE composition_equipe(
+    id_match INT NOT NULL,
+    id_equipe INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_equipe) REFERENCES equipe(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id)
 );
+
+
+
+
+CREATE TABLE tir(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id)
+);
+
+
+CREATE TABLE deux_pt(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id) 
+);
+
+
+CREATE TABLE trois_pt(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id) 
+);
+
+
+-- stat_rebond_match :
+--     id_match
+--     id_joueur
+--     minute
+
+CREATE TABLE stat_rebond_match(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id)
+);
+-- Ajout des statistiques de rebond pour des joueurs lors du match 1 (données de test)
+
+-- Ajoutez d'autres statistiques de rebond pour d'autres matchs en fonction des IDs réels des joueurs et des matchs.
+
+
+-- stat_passe_decisive_match :
+--     id_match   
+--     id_joueur
+
+CREATE TABLE stat_passe_decisive_match(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id)
+);
+
+CREATE TABLE stat_interception_match(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id)
+);
+
+
+-- detail_entree_sortie_joueur :
+--     id_match
+--     id_joueur
+--     minute_entree
+--     minute_sortie
+
+-- calc minute joue
+CREATE TABLE detail_entree_sortie_joueur(
+    id_match INT NOT NULL,
+    id_joueur INT NOT NULL,
+    minute_entree TIMESTAMP,
+    minute_sortie TIMESTAMP,
+    FOREIGN KEY(id_match) REFERENCES match(id),
+    FOREIGN KEY(id_joueur) REFERENCES joueur(id)
+);
+
+
+CREATE OR REPLACE FUNCTION get_team_stats(team_id INT)
+RETURNS TABLE(
+    id_match INT,
+    id_joueur INT,
+    joueur VARCHAR(255),
+    debut TIMESTAMP,
+    fin TIMESTAMP,
+    tir BIGINT,
+    deux_pts BIGINT,
+    trois_pts BIGINT,
+    rebond BIGINT,
+    passe_decisive BIGINT,
+    interception BIGINT,
+    minute_jouee NUMERIC
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        composition_equipe.id_match, 
+        composition_equipe.id_joueur, 
+        j.nom,
+        m.date_heure_debut,
+        m.date_heure_fin,
+        (SELECT COUNT(*) FROM tir WHERE tir.id_match = composition_equipe.id_match AND tir.id_joueur = composition_equipe.id_joueur),
+        (SELECT COUNT(*) FROM deux_pt WHERE deux_pt.id_match = composition_equipe.id_match AND deux_pt.id_joueur = composition_equipe.id_joueur),
+        (SELECT COUNT(*) FROM trois_pt WHERE trois_pt.id_match = composition_equipe.id_match AND trois_pt.id_joueur = composition_equipe.id_joueur),
+        (SELECT COUNT(*) FROM stat_rebond_match WHERE stat_rebond_match.id_match = composition_equipe.id_match AND stat_rebond_match.id_joueur = composition_equipe.id_joueur),
+        (SELECT COUNT(*) FROM stat_passe_decisive_match WHERE stat_passe_decisive_match.id_match = composition_equipe.id_match AND stat_passe_decisive_match.id_joueur = composition_equipe.id_joueur),
+        (SELECT COUNT(*) FROM stat_interception_match WHERE stat_interception_match.id_match = composition_equipe.id_match AND stat_interception_match.id_joueur = composition_equipe.id_joueur),
+        (SELECT SUM(ABS(EXTRACT(EPOCH FROM (detail_entree_sortie_joueur.minute_sortie - detail_entree_sortie_joueur.minute_entree))/60)) FROM detail_entree_sortie_joueur WHERE detail_entree_sortie_joueur.id_match = composition_equipe.id_match AND detail_entree_sortie_joueur.id_joueur = composition_equipe.id_joueur)
+    FROM 
+        composition_equipe
+    JOIN joueur AS j ON j.id = composition_equipe.id_joueur
+    JOIN match AS m ON m.id = composition_equipe.id_match
+    WHERE 
+        composition_equipe.id_equipe = team_id;
+END; 
+$$;
--- src/main/java/org/itu/nba/controller/EquipeController.java
+++ src/main/java/org/itu/nba/controller/EquipeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/equipes")
+@CrossOrigin
 public class EquipeController {
 
     @Autowired
